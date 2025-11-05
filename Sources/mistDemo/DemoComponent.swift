1fc282962471a06e4a8574de630507c218189225
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-05 14:42:10 +0100

    test mist 0.9.0

--- Sources/mistDemo/DemoComponent.swift
+++ Sources/mistDemo/DemoComponent.swift
@@ -7,3 +7,34 @@ struct DemoComponent: Mist.Component
     let models: [any Mist.Model.Type] = [DemoModel1.self, DemoModel2.self]
     let template: String = "/mistDemo/DemoComponent"
 }
+
+struct DemoComponentInline: Mist.Component
+{
+    let models: [any Mist.Model.Type] = [DemoModel1.self, DemoModel2.self]
+    let templateSource: String? =
+        """
+        <tr
+            class="hover:bg-red-50 dark:hover:bg-neutral-750 transition-colors duration-150"
+            mist-component="DemoComponent"
+            mist-id="#(component.demomodel1.id)"
+        >
+            <td class="px-6 py-4">
+                <span class="font-mono text-indigo-600 dark:text-indigo-400 text-sm">
+                    #(component.demomodel1.id)
+                </span>
+            </td>
+
+            <td class="px-6 py-4 max-w-[160px]">
+                <span class="block text-sm text-gray-700 dark:text-neutral-300 truncate font-medium">
+                    #(component.demomodel1.text)
+                </span>
+            </td>
+
+            <td class="px-6 py-4">
+                <span class="text-sm text-gray-700 dark:text-neutral-300 font-medium">
+                    #(component.demomodel2.text)
+                </span>
+            </td>
+        </tr>
+        """
+}
