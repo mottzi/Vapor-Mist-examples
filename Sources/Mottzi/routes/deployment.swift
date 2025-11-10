9d6b17898da92115d9a83954718fcb469ae0ee57
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-10 11:06:06 +0100

    test

--- Sources/Mottzi/routes/deployment.swift
+++ Sources/Mottzi/routes/deployment.swift
@@ -5,7 +5,7 @@ extension Application
     // registers test route for demo purposes: www.mottzi.de/test
     public func initTestRoute()
     {
-        self.get("test") { _ in "Test response string: 0" }
+        self.get("test") { _ in "Test response string: 1" }
     }
     
     // initializes github webhook handling
