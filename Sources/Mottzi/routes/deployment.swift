ae378ec003953f5db011c808c76e9a69875f69ff
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-12 22:30:57 +0100

    1

--- Sources/Mottzi/routes/deployment.swift
+++ Sources/Mottzi/routes/deployment.swift
@@ -7,7 +7,7 @@ extension Application
 {
     public func initTestRoute()
     {
-        self.get("test") { _ in "Test response string: 10" }
+        self.get("test") { _ in "Test response string: 1" }
     }
     
     func initPushWebhook()
