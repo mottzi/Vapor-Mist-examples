d11fed0a2bdc29b07264a588915a0b6ec231484b
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-12 23:11:30 +0100

    working1.1

--- Sources/Mottzi/routes/deployment.swift
+++ Sources/Mottzi/routes/deployment.swift
@@ -7,7 +7,7 @@ extension Application
 {
     public func initTestRoute()
     {
-        self.get("test") { _ in "Test response string: working1" }
+        self.get("test") { _ in "Test response string: working1.1" }
     }
     
     func initPushWebhook()
