bb6b37027c497061663f24a689966ce09f80b957
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-14 19:43:16 +0100

    haha

--- Sources/Mist/MistMessage.swift
+++ Sources/Mist/MistMessage.swift
@@ -22,11 +22,85 @@ enum ComponentMessage: Codable {
     case create(component: String, id: UUID, html: String)
     case update(component: String, id: UUID, html: String)
     case delete(component: String, id: UUID)
+    
+    enum CodingKeys: String, CodingKey {
+        case create, update, delete
+    }
+    
+    struct ComponentPayload: Codable {
+        let component: String
+        let id: UUID
+        let html: String
+    }
+    
+    struct DeletePayload: Codable {
+        let component: String
+        let id: UUID
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .create(let component, let id, let html):
+            try container.encode(ComponentPayload(component: component, id: id, html: html), forKey: .create)
+        case .update(let component, let id, let html):
+            try container.encode(ComponentPayload(component: component, id: id, html: html), forKey: .update)
+        case .delete(let component, let id):
+            try container.encode(DeletePayload(component: component, id: id), forKey: .delete)
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let value = try? container.decode(ComponentPayload.self, forKey: .create) {
+            self = .create(component: value.component, id: value.id, html: value.html)
+        } else if let value = try? container.decode(ComponentPayload.self, forKey: .update) {
+            self = .update(component: value.component, id: value.id, html: value.html)
+        } else if let value = try? container.decode(DeletePayload.self, forKey: .delete) {
+            self = .delete(component: value.component, id: value.id)
+        } else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Unknown case"))
+        }
+    }
 }
 
 enum QueryComponentMessage: Codable {
-    case upsert(component: String, html: String) // Renamed from 'create' for clarity
+    case upsert(component: String, html: String)
     case delete(component: String)
+    
+    enum CodingKeys: String, CodingKey {
+        case upsert, delete
+    }
+    
+    struct UpsertPayload: Codable {
+        let component: String
+        let html: String
+    }
+    
+    struct DeletePayload: Codable {
+        let component: String
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .upsert(let component, let html):
+            try container.encode(UpsertPayload(component: component, html: html), forKey: .upsert)
+        case .delete(let component):
+            try container.encode(DeletePayload(component: component), forKey: .delete)
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let value = try? container.decode(UpsertPayload.self, forKey: .upsert) {
+            self = .upsert(component: value.component, html: value.html)
+        } else if let value = try? container.decode(DeletePayload.self, forKey: .delete) {
+            self = .delete(component: value.component)
+        } else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Unknown case"))
+        }
+    }
 }
 
 extension Clients
