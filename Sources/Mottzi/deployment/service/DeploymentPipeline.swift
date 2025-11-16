ea13f2c35db4ad198fd338c9e016eaedc4fdf379
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-16 14:46:33 +0100

    swift concurrency on execute()

---
+++ Sources/Mottzi/deployment/service/DeploymentPipeline.swift
@@ -0,0 +1,195 @@
+import Vapor
+import Fluent
+
+extension Deployment
+{
+    /// Handles deployment process execution (pull, build, queue check, restart).
+    /// Ensures chronological execution by re-running latest canceled commit after successfull deployments.
+    struct Pipeline
+    {
+        /// Creates and processes a new `Deployment`. After successfull deployment, this will check for previously
+        /// cancelled deployments and re-runs the latest one found.
+        ///
+        /// - Parameter message: The commit message of this deployment
+        /// - Note: This is called when a valid GitHub pushevent is received.
+        public static func start(message: String?, on database: Database) async
+        {
+            await deploy(message: message, on: database)
+        }
+        
+        /// Re-runs an existing `Deployment`.
+        ///
+        /// - Parameter deployment: Deployment to re-run
+        /// - Note: This is called on the latest cancelled deployment whenever any deployment finishes successfully.
+        private static func resume(existingDeployment: Deployment, on database: Database) async
+        {
+            await deploy(existingDeployment: existingDeployment, on: database)
+        }
+        
+        /// Internal recursive deployment pipeline. It can re-process exisiting deployments or create and process new deployments.
+        ///
+        /// - Parameters:
+        ///   - existingDeployment: Pass a deployment to re-run it.
+        ///   - message: Pass a commit message for newly created deployments.
+        private static func deploy(existingDeployment: Deployment? = nil, message: String? = nil, on database: Database) async
+        {
+            let canDeploy = await Manager.shared.requestPipeline()
+            
+            let deployment: Deployment
+            
+            if let existingDeployment
+            {
+                deployment = existingDeployment
+                deployment.startedAt = .now
+                deployment.status = canDeploy ? "running" : "canceled"
+            }
+            else
+            {
+                deployment = Deployment(
+                    status: canDeploy ? "running" : "canceled",
+                    message: message ?? ""
+                )
+            }
+            
+            try? await deployment.save(on: database)
+            
+            guard canDeploy else { return }
+            
+            do
+            {
+                try await pull()
+                try await build()
+                try await move()
+                
+                deployment.status = "success"
+                deployment.finishedAt = .now
+                try? await deployment.save(on: database)
+                await Deployment.Pipeline.Manager.shared.endDeployment()
+                
+                let canceledDeployment = try await Deployment.query(on: database)
+                    .filter(\.$status, .equal, "canceled")
+                    .filter(\.$startedAt, .greaterThan, deployment.startedAt)
+                    .sort(\.$startedAt, .descending)
+                    .first()
+                
+                if let canceledDeployment
+                {
+                    await resume(existingDeployment: canceledDeployment, on: database)
+                }
+                else
+                {
+                    try await deployment.setCurrent(on: database)
+                    try await restart()
+                }
+            }
+            catch
+            {
+                deployment.status = "failed"
+                deployment.finishedAt = .now
+                try? await deployment.save(on: database)
+                await Deployment.Pipeline.Manager.shared.endDeployment()
+                Logger(label: "mottzi").error("failed with error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+extension Deployment.Pipeline
+{
+    private static func execute(_ command: String, step: Int) async throws
+    {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+        process.arguments = ["bash", "-c", command]
+        process.currentDirectoryURL = URL(fileURLWithPath: "/var/www/mottzi")
+        
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        process.standardError = outputPipe
+        
+        try process.run()
+        process.waitUntilExit()
+        
+        if process.terminationStatus != 0
+        {
+            let output = try outputPipe.fileHandleForReading.readToEnd()
+            let str = String(data: output ?? Data(), encoding: .utf8)
+            
+            throw NSError(domain: "DeploymentError", code: step, userInfo: [NSLocalizedDescriptionKey: "Command failed: \(command), Output: \(str ?? "")"])
+        }
+    }
+    
+    private static func pull() async throws
+    {
+        try await execute("git pull", step: 1)
+    }
+    
+    private static func build() async throws
+    {
+        try await execute("swift build -c debug", step: 2)
+    }
+    
+    private static func restart() async throws
+    {
+        try await execute("supervisorctl restart mottzi", step: 4)
+    }
+    
+    private static func move() async throws
+    {
+        let fileManager = FileManager.default
+        let buildPath = "/var/www/mottzi/.build/debug/Mottzi"
+        let deployPath = "/var/www/mottzi/deploy/Mottzi"
+        
+        do
+        {
+            try fileManager.createDirectory(atPath: "/var/www/mottzi/deploy", withIntermediateDirectories: true)
+            
+            if fileManager.fileExists(atPath: deployPath)
+            {
+                try fileManager.removeItem(atPath: deployPath)
+            }
+            
+            try fileManager.moveItem(atPath: buildPath, toPath: deployPath)
+        }
+        catch
+        {
+            throw error
+        }
+    }
+    
+    public static func getCommitMessage(of request: Request) -> String?
+    {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard let bodyString = request.body.string,
+              let jsonData = bodyString.data(using: .utf8),
+              let payload = try? decoder.decode(Deployment.Webhook.Payload.self, from: jsonData)
+        else { return nil }
+        
+        return payload.headCommit.message
+    }
+}
+
+extension Deployment.Pipeline
+{
+    actor Manager
+    {
+        static let shared = Manager()
+        private init() {}
+        
+        private(set) var isDeploying: Bool = false
+        
+        func requestPipeline() async -> Bool
+        {
+            guard isDeploying == false else { return false }
+            isDeploying = true
+            return true
+        }
+        
+        func endDeployment() async
+        {
+            isDeploying = false
+        }
+    }
+}
---
+++ Sources/Mottzi/deployment/service/DeploymentWebhook.swift
@@ -0,0 +1,88 @@
+import Vapor
+
+extension Application
+{
+    func useWebhook()
+    {
+        Deployment.Webhook.register("pushevent", on: self)
+        { request async in
+            let message = Deployment.Pipeline.getCommitMessage(of: request)
+            await Deployment.Pipeline.start(message: message, on: request.db)
+        }
+    }
+}
+
+extension Deployment
+{
+    struct Webhook
+    {
+        static func register(_ endpoint: PathComponent..., on app: Application, action: @Sendable @escaping (Request) async -> ())
+        {
+            let accepted = Response(status: .ok, body: .init(stringLiteral: "[mottzi] Push event accepted."))
+            let denied = Response(status: .forbidden, body: .init(stringLiteral: "[mottzi] Push event denied."))
+                
+            app.post(endpoint)
+            { request async -> Response in
+                guard validateSignature(of: request) else { return denied }
+                Task.detached { await action(request) }
+                return accepted
+            }
+        }
+        
+        private static func validateSignature(of request: Request) -> Bool
+        {
+            let secret = Environment.Variables.GITHUB_WEBHOOK_SECRET.value
+            guard let secretData = secret.data(using: .utf8) else { return false }
+
+            guard let signatureHeader = request.headers.first(name: "X-Hub-Signature-256") else { return false }
+            guard signatureHeader.hasPrefix("sha256=") else { return false }
+            let signatureHex = String(signatureHeader.dropFirst("sha256=".count))
+            
+            guard let payload = request.body.string else { return false }
+            guard let payloadData = payload.data(using: .utf8) else { return false }
+            
+            let secretDataKey = SymmetricKey(data: secretData)
+            let signature = HMAC<SHA256>.authenticationCode(for: payloadData, using: secretDataKey)
+            let expectedSignatureHex = signature.map { String(format: "%02x", $0) }.joined()
+            guard expectedSignatureHex.count == signatureHex.count else { return false }
+            
+            return HMAC<SHA256>.isValidAuthenticationCode(
+                signatureHex.hexadecimal ?? Data(),
+                authenticating: payloadData,
+                using: secretDataKey
+            )
+        }
+    }
+}
+
+extension Deployment.Webhook
+{
+    struct Payload: Codable
+    {
+        let headCommit: Commit
+        
+        struct Commit: Codable
+        {
+            let id: String
+            let message: String
+        }
+    }
+}
+
+extension String
+{
+    var hexadecimal: Data?
+    {
+        var data = Data(capacity: count / 2)
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        
+        regex.enumerateMatches(in: self, range: NSRange(startIndex..., in: self))
+        { match, _, _ in
+            let byteString = (self as NSString).substring(with: match!.range)
+            let num = UInt8(byteString, radix: 16)!
+            data.append(num)
+        }
+        
+        return data
+    }
+}
