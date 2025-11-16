a05c7f944931e3966cf0e0df86db89ebf6ade44f
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-16 16:04:36 +0100

    1

--- Sources/Mottzi/deployment/routes/DeploymentRoutes.swift
+++ Sources/Mottzi/deployment/routes/DeploymentRoutes.swift
@@ -2,12 +2,12 @@ import Vapor
 import Mist
 
 typealias MistModelContainer = Mist.ModelContainer
-
+1
 extension Application
 {
     func useTestRoute()
     {
-        self.get("test") { _ in "Test response hehe" }
+        self.get("test") { _ in "Test response 1" }
     }
     
     func useDeployPanel()
