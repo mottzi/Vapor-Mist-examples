f47c7b7d76e8984c0f32b386dc20ce0010cbcf99
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-18 23:40:24 +0100

    15

--- Sources/Mottzi/deployment/routes/DeploymentRoutes.swift
+++ Sources/Mottzi/deployment/routes/DeploymentRoutes.swift
@@ -7,7 +7,7 @@ extension Application
 {
     func useTestRoute()
     {
-        self.get("test") { _ in "Test response 13" }
+        self.get("test") { _ in "Test response 15" }
     }
     
     func useDeployPanel()
