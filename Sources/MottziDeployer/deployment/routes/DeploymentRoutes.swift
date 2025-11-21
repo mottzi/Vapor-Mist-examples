7a288dd39847b02859d092dc343f49f51aee74fb
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-21 11:31:10 +0100

    1

--- Sources/MottziDeployer/deployment/routes/DeploymentRoutes.swift
+++ Sources/MottziDeployer/deployment/routes/DeploymentRoutes.swift
@@ -1,43 +1,38 @@
-import Vapor
 import Mist
+import Vapor
 
 typealias MistModelContainer = Mist.ModelContainer
 
-extension Application
-{
-    func useTestRoute()
-    {
-        self.get("test") { _ in "Test response 3" }
+extension Application {
+    func useTestRoute() {
+        self.get("test") { _ in "Test response 1" }
     }
-    
-    func useDeployPanel()
-    {
-        self.get("deployment")
-        { request async throws -> View in
-            
+
+    func useDeployPanel() {
+        self.get("deployment") { request async throws -> View in
+
             let componentsContext = await DeploymentRow().makeContext(ofAll: request.db)
             let currentDeployment = try? await Deployment.getCurrent(on: request.db)
-            
+
             // Build the status component context (matching Mist's structure)
             var statusComponent: MistModelContainer?
-            
+
             if let currentDeployment {
                 var container = MistModelContainer()
                 container.add(currentDeployment, for: "deployment")
                 statusComponent = container
             }
-        
-            struct DeploymentPanelContext: Encodable
-            {
+
+            struct DeploymentPanelContext: Encodable {
                 let components: [MistModelContainer]
                 let component: MistModelContainer?
             }
-            
+
             let context = DeploymentPanelContext(
                 components: componentsContext.components,
                 component: statusComponent
             )
-            
+
             return try await request.view.render("deployment/DeploymentPanel", context)
         }
     }
