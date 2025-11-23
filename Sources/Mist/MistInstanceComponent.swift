ab306360055ec61762966a01e4c11e997fc0dffe
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-23 22:10:04 +0100

    1

--- Sources/Mist/MistInstanceComponent.swift
+++ Sources/Mist/MistInstanceComponent.swift
@@ -1,30 +1,24 @@
-import Vapor
 import Fluent
+import Vapor
 
-public protocol InstanceComponent: Component
-{
+public protocol InstanceComponent: Component {
     func allModels(on db: Database) async -> [any Model]?
 }
 
-public extension InstanceComponent
-{
-    func allModels(on db: Database) async -> [any Model]?
-    {
+extension InstanceComponent {
+    public func allModels(on db: Database) async -> [any Model]? {
         guard let primaryModelType = models.first else { return nil }
         return await primaryModelType.findAll(on: db)
     }
 }
 
-public extension InstanceComponent
-{
-    func makeContext(ofAll db: Database) async -> MultipleComponentContext
-    {
+extension InstanceComponent {
+    public func makeContext(ofAll db: Database) async -> MultipleComponentContext {
         var modelContainers: [ModelContainer] = []
 
         guard let primaryModels = await allModels(on: db) else { return .empty }
 
-        for primaryModel in primaryModels
-        {
+        for primaryModel in primaryModels {
             guard let modelID = primaryModel.id else { continue }
             guard let modelContext = await makeContext(of: modelID, in: db) else { continue }
 
@@ -37,41 +31,41 @@ public extension InstanceComponent
     }
 }
 
-public extension InstanceComponent
-{
-    func handleCreate(id: UUID, app: Application) async
-    {
-        for subscriber in await app.mist.clients.subscribers(of: name)
-        {
-            Task.detached
-            {
-                let state = await app.mist.clients.state(for: subscriber.id, componentID: id.uuidString, default: defaultState)
-                guard let html = await render(id: id, state: state, on: app.db, using: app.leaf.renderer) else { return }
-                await app.mist.clients.send(Message.InstanceCreate(component: name, id: id, html: html), to: subscriber.id)
+extension InstanceComponent {
+    public func handleCreate(id: UUID, app: Application) async {
+        for subscriber in await app.mist.clients.subscribers(of: name) {
+            Task.detached {
+                let state = await app.mist.clients.state(
+                    for: subscriber.id, componentID: id.uuidString, default: defaultState)
+                guard
+                    let html = await render(
+                        id: id, state: state, on: app.db, using: app.leaf.renderer)
+                else { return }
+                await app.mist.clients.send(
+                    Message.InstanceCreate(component: name, id: id, html: html), to: subscriber.id)
             }
         }
     }
 
-    func handleUpdate(id: UUID, app: Application) async
-    {
-        for subscriber in await app.mist.clients.subscribers(of: name)
-        {
-            Task.detached
-            {
-                let state = await app.mist.clients.state(for: subscriber.id, componentID: id.uuidString, default: defaultState)
-                guard let html = await render(id: id, state: state, on: app.db, using: app.leaf.renderer) else { return }
-                await app.mist.clients.send(Message.InstanceUpdate(component: name, id: id, html: html), to: subscriber.id)
+    public func handleUpdate(id: UUID, app: Application) async {
+        for subscriber in await app.mist.clients.subscribers(of: name) {
+            Task.detached {
+                let state = await app.mist.clients.state(
+                    for: subscriber.id, componentID: id.uuidString, default: defaultState)
+                guard
+                    let html = await render(
+                        id: id, state: state, on: app.db, using: app.leaf.renderer)
+                else { return }
+                await app.mist.clients.send(
+                    Message.InstanceUpdate(component: name, id: id, html: html), to: subscriber.id)
             }
         }
     }
 
-    func handleDelete(id: UUID, app: Application) async
-    {
-        Task.detached
-        {
+    public func handleDelete(id: UUID, app: Application) async {
+        Task.detached {
             await app.mist.clients.clearState(for: id.uuidString)
             await app.mist.clients.broadcast(Message.InstanceDelete(component: name, id: id))
         }
     }
 }
-
