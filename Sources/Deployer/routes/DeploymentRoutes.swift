cc6c8d1b16d07c4544a7460a308838f326133965
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-25 21:14:28 +0100

    1

--- Sources/Deployer/routes/DeploymentRoutes.swift
+++ Sources/Deployer/routes/DeploymentRoutes.swift
@@ -8,30 +8,29 @@ extension Application
     func useDeployPanel() 
     {
         self.get("Deployer")
-        { request async throws -> String in
-            return "hi"
-//            let componentsContext = await DeploymentRow().makeContext(ofAll: request.db)
-//            let currentDeployment = try? await Deployment.getCurrent(on: request.db)
-//
-//            let statusComponent = currentDeployment.map 
-//            { 
-//                var container = MistModelContainer()
-//                container.add($0, for: "deployment")
-//                return container
-//             }
-//
-//            struct DeploymentPanelContext: Encodable 
-//            {
-//                let components: [MistModelContainer]
-//                let component: MistModelContainer?
-//            }
-//
-//            let context = DeploymentPanelContext(
-//                components: componentsContext.components,
-//                component: statusComponent
-//            )
-//
-//            return try await request.view.render("Deployer/DeploymentPanel", context)
+        { request async throws -> View in
+            let componentsContext = await DeploymentRow().makeContext(ofAll: request.db)
+            let currentDeployment = try? await Deployment.getCurrent(on: request.db)
+
+            let statusComponent = currentDeployment.map 
+            { 
+                var container = MistModelContainer()
+                container.add($0, for: "deployment")
+                return container
+             }
+
+            struct DeploymentPanelContext: Encodable 
+            {
+                let components: [MistModelContainer]
+                let component: MistModelContainer?
+            }
+
+            let context = DeploymentPanelContext(
+                components: componentsContext.components,
+                component: statusComponent
+            )
+
+            return try await request.view.render("Deployer/DeploymentPanel", context)
         }
     }
 }
