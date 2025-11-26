edab72e76c03723ebb40e6b75f9be7ff4a45aed0
Berken Sayilir <sayilir.berken@gmail.com>
2025-11-26 15:26:14 +0100

    deploy command robustness

--- Sources/Deployer/commands/DeploymentCommands.swift
+++ Sources/Deployer/commands/DeploymentCommands.swift
@@ -8,6 +8,11 @@ struct DeployCommand: AsyncCommand
 
     func run(using context: CommandContext, signature: Signature) async throws
     {
+        guard await Deployment.Pipeline.Manager.shared.requestPipeline() else {
+            context.console.print("⚠️  Deployment rejected: Another deployment is currently in progress.")
+            return
+        }
+        
         context.console.print("\nStart deploying...")
 
         let pipeline = Deployment.Pipeline(
@@ -31,9 +36,11 @@ struct DeployCommand: AsyncCommand
             try await pipeline.restart()
             
             context.console.print("Successfully deployed.\n")
+            await Deployment.Pipeline.Manager.shared.endDeployment()
         }
         catch {
             context.console.print("Error deploying: \(error.localizedDescription).\n")
+            await Deployment.Pipeline.Manager.shared.endDeployment()
         }
     }
 }
