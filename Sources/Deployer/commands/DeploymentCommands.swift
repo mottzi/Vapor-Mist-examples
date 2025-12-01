2548a0ab1543fa2258f65aa9cf5457d6faeaae62
Berken Sayilir <sayilir.berken@gmail.com>
2025-12-01 10:37:24 +0100

    new deployer

--- Sources/Deployer/commands/DeploymentCommands.swift
+++ Sources/Deployer/commands/DeploymentCommands.swift
@@ -8,22 +8,24 @@ struct DeployCommand: AsyncCommand
 
     func run(using context: CommandContext, signature: Signature) async throws
     {
-        // wont work!!!! vapor commands are different processes
-        guard await Deployment.Pipeline.Manager.shared.requestPipeline() else {
-            context.console.print("⚠️  Deployment rejected: Another deployment is currently in progress.")
-            return
-        }
+        let runningCount = try await Deployment.query(on: context.application.db)
+            .filter(\.$status, .equal, "running")
+            .filter(\.$startedAt, .greaterThanOrEqual, Date.now.addingTimeInterval(-1800))
+            .count()
         
+        guard runningCount == 0 else { return context.console.print("⚠️  Deployment rejected: Another deployment is currently in progress.") }
+                
         context.console.print("\nStart deploying...")
 
-        let pipeline = Deployment.Pipeline(
-            config: .init(
-                productName: "Deployer",
-                supervisorJob: "deployer"
-            )
+        let config = Deployment.Configuration(
+            productName: "Deployer",
+            supervisorJob: "deployer"
         )
         
-        do {
+        let pipeline = Deployment.Pipeline(config: config)
+        
+        do
+        {
             context.console.print("    #1 Pull Deployer")
             try await pipeline.pull()
             
@@ -39,7 +41,8 @@ struct DeployCommand: AsyncCommand
             context.console.print("Successfully deployed.\n")
             await Deployment.Pipeline.Manager.shared.endDeployment()
         }
-        catch {
+        catch
+        {
             context.console.print("Error deploying: \(error.localizedDescription).\n")
             await Deployment.Pipeline.Manager.shared.endDeployment()
         }
