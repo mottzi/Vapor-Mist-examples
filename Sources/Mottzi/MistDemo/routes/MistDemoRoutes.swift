73d31203ee24a5d922a7f4a3d3b1cee3f829f24c
Berken Sayilir <sayilir.berken@gmail.com>
2025-12-04 14:15:15 +0100

    1

--- Sources/Mottzi/MistDemo/routes/MistDemoRoutes.swift
+++ Sources/Mottzi/MistDemo/routes/MistDemoRoutes.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Application {
     func useMistDemo() {
         self.get("test") { _ in
-            return "3"
+            return "1"
         }
 
         self.get("MistDemo")
