f467fff4b248c45a452bfa645a22e02b65712b92
Berken Sayilir <sayilir.berken@gmail.com>
2025-12-04 20:46:23 +0100

    1

--- Sources/Deployer/commands/DeploymentCommands.swift
+++ Sources/Deployer/commands/DeploymentCommands.swift
@@ -1,20 +1,17 @@
 import Vapor
 
-struct DeployCommand: AsyncCommand
-{
+struct DeployCommand: AsyncCommand {
     struct Signature: CommandSignature {}
 
-    let help: String = "111 Pulls, builds, moves and restarts Deployer."
+    let help: String = "Pulls, builds, moves and restarts Deployer."
 
-    func run(using context: CommandContext, signature: Signature) async throws
-    {
+    func run(using context: CommandContext, signature: Signature) async throws {
         let uri = URI(string: "http://localhost:8081/Deployer/deploy")
-    
-        let response = try await context.application.client.post(uri) 
-        { request in
+
+        let response = try await context.application.client.post(uri) { request in
             request.headers.add(name: "X-Deploy-Secret", value: "4132Muttenz")
         }
-    
+
         context.console.print("Deployer Response: \(response.status).")
     }
 }
--- Sources/Deployer/components/DeploymentStatus.swift
+++ Sources/Deployer/components/DeploymentStatus.swift
@@ -1,15 +1,12 @@
-import Vapor
 import Fluent
 import Mist
+import Vapor
 
-struct DeploymentStatus: QueryComponent
-{
+struct DeploymentStatus: QueryComponent {
     let models: [any Mist.Model.Type] = [Deployment.self]
     let template: Template = .file(path: "Deployer/DeploymentStatus")
-    
-    func queryModel(on db: Database) async -> (any Mist.Model)?
-    {
-        return try? await Deployment.getCurrent(named: "Deployer", on: db)
+
+    func queryModel(on db: Database) async -> (any Mist.Model)? {
+        return try? await Deployment.getCurrent(named: "Mottzi", on: db)
     }
 }
-
