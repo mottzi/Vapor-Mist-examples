390516f33641b6ba110b52b46e91a15f44dc8d25
Berken Sayilir <sayilir.berken@gmail.com>
2025-12-05 12:18:07 +0100

    remote mist

--- Package.swift
+++ Package.swift
@@ -7,16 +7,14 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.12.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
+        .package(url: "https://github.com/mottzi/Vapor-Mist.git", from: "0.14.15"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.8.0"),
     ],
     targets: [
         .executableTarget(
             name: "Mottzi",
             dependencies: [
-                "Mist",
+                .product(name: "Mist", package: "Vapor-Mist"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             ],
             swiftSettings: swiftSettings
@@ -24,25 +22,10 @@ let package = Package(
         .executableTarget(
             name: "Deployer",
             dependencies: [
-                "Mist",
+                .product(name: "Mist", package: "Vapor-Mist"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             ],
             swiftSettings: swiftSettings
-        ),
-        .target(
-            name: "Mist",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Fluent", package: "fluent"),
-                .product(name: "Leaf", package: "leaf"),
-            ]
-        ),
-        .testTarget(
-            name: "MistTests",
-            dependencies: [
-                "Mist",
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            ]
         ),
     ],
     swiftLanguageModes: [.v5]
