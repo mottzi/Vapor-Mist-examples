fac8808998ea0d34adda277a67e768107eadda8a
Berken Sayilir <sayilir.berken@gmail.com>
2026-04-28 14:05:54 +0200

    minimal reproducable example

--- Package.swift
+++ Package.swift
@@ -7,25 +7,18 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/mottzi/Vapor-Mist.git", from: "0.14.15"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.8.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
     ],
     targets: [
         .executableTarget(
             name: "mottzi",
             dependencies: [
-                .product(name: "Mist", package: "Vapor-Mist"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Vapor", package: "vapor"),
             ],
-            swiftSettings: swiftSettings
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+            ]
         ),
     ],
     swiftLanguageModes: [.v5]
 )
-
-var swiftSettings: [SwiftSetting] {
-    [
-        .enableUpcomingFeature("DisableOutwardActorInference"),
-        .enableExperimentalFeature("StrictConcurrency"),
-    ]
-}
