db22f131cce33e433669066f18662265799d62fe
Berken Sayilir <sayilir.berken@gmail.com>
2026-05-03 13:56:45 +0200

    fix

--- Sources/Mottzi/MistExamples/CounterExample/CounterPage.swift
+++ Sources/Mottzi/MistExamples/CounterExample/CounterPage.swift
@@ -1,29 +1,29 @@
-import Elementary
-
-struct CounterPage: HTMLDocument {
-    
-    var title = "Counter Example"
-
-    var head: some HTML {
-        meta(.name(.description), .content("Typesafe HTML in modern Swift"))
-        link(.rel(.stylesheet), .href("/mistexamples.css"))
-    }
-
-    var body: some HTML {
-        
-        main(.class("container")) {
-            a(.href("/MistExamples"), .class("back-link")) { "← Back to Examples" }
-            
-            header(.class("mb-4")) {
-                h1 { "Counter Example" }
-                p(.class("desc")) { "A global counter demonstrating real-time manual state updates across all connected clients." }
-            }
-            
-            CounterComponent()
-                .body(state: CounterState())
-        }
-        
-        script(.src("/morphdom.js")) {}
-        script(.src("/mist.js")) {}
-    }
-}
+//import Elementary
+//
+//struct CounterPage: HTMLDocument {
+//    
+//    var title = "Counter Example"
+//
+//    var head: some HTML {
+//        meta(.name(.description), .content("Typesafe HTML in modern Swift"))
+//        link(.rel(.stylesheet), .href("/mistexamples.css"))
+//    }
+//
+//    var body: some HTML {
+//        
+//        main(.class("container")) {
+//            a(.href("/MistExamples"), .class("back-link")) { "← Back to Examples" }
+//            
+//            header(.class("mb-4")) {
+//                h1 { "Counter Example" }
+//                p(.class("desc")) { "A global counter demonstrating real-time manual state updates across all connected clients." }
+//            }
+//            
+//            CounterComponent()
+//                .body(state: CounterState())
+//        }
+//        
+//        script(.src("/morphdom.js")) {}
+//        script(.src("/mist.js")) {}
+//    }
+//}
