b39f24f6aacbfd6b08a36b5cab3852f6597e4896
Berken Sayilir <sayilir.berken@gmail.com>
2026-05-03 14:00:46 +0200

    test

--- Sources/Mottzi/MistExamples/CounterExample/CounterComponent.swift
+++ Sources/Mottzi/MistExamples/CounterExample/CounterComponent.swift
@@ -12,33 +12,33 @@ struct CounterComponent: ManualComponent {
     let state = LiveState(of: CounterState())
 
     // Called when... to..., (optional: resulting in...).
-//    func body(state: CounterState) -> some HTML {
-//        
-//        div(
-//            .mistComponent(value: name),
-//            .class("card stack text-center mx-auto max-w-sm")
-//        ) {
-//            h2 {
-//                "Global Count"
-//            }
-//            div(.class("text-huge")) {
-//                "\(state.count)"
-//            }
-//            button(.mistAction(value: "increment")) {
-//                "Increment Count"
-//            }
-//        }
-//    }
-    
-    var template: any Template {
-        LeafTemplate.inline("""
-        <div mist-component="CounterComponent" class="card stack text-center mx-auto max-w-sm">
-            <h2>Global Count</h2>
-            <div class="text-huge">#(count)</div>
-            <button mist-action="increment">Increment Count</button>
-        </div>
-        """)
+    func body(state: CounterState) -> some HTML {
+        
+        div(
+            .mistComponent(value: name),
+            .class("card stack text-center mx-auto max-w-sm")
+        ) {
+            h2 {
+                "Global Count"
+            }
+            div(.class("text-huge")) {
+                "\(state.count)"
+            }
+            button(.mistAction(value: "increment")) {
+                "Increment Count"
+            }
+        }
     }
+    
+//    var template: any Template {
+//        LeafTemplate.inline("""
+//        <div mist-component="CounterComponent" class="card stack text-center mx-auto max-w-sm">
+//            <h2>Global Count</h2>
+//            <div class="text-huge">#(count)</div>
+//            <button mist-action="increment">Increment Count</button>
+//        </div>
+//        """)
+//    }
     
 //    var template: any Template { LeafTemplate.file("CounterComponent") }
     
