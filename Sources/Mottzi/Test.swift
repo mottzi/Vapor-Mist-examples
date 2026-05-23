12fd7b2783f304f9d56416a8703d0db9cecbf171
mottzi <sayilir.berken@gmail.com>
2026-05-23 11:47:24 +0200

    feat: new feature (error)

--- Sources/Mottzi/Test.swift
+++ Sources/Mottzi/Test.swift
@@ -7,9 +7,12 @@ extension Application {
     func registerTestRoute() {
   
         // This is the test route to demo code changes going live.
-
+        This won't even compile!
         self.get("test") { _ in
-            "We made some code changes! Lets deploy the latest version!"
+            """
+            We made some code changes! Lets deploy the latest version!
+            Let's make some additional changes, but this time, we include a error!
+            """
         }
 
     }
