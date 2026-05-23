25ccadb3ef25ba6225248a2e04a8db869ddd4a4e
mottzi <sayilir.berken@gmail.com>
2026-05-23 11:45:51 +0200

    feat: new feature

--- Sources/Mottzi/Test.swift
+++ Sources/Mottzi/Test.swift
@@ -1,11 +1,15 @@
 import Vapor
 
+// This is the Swift app that Deployer will manage.
+
 extension Application {
 
     func registerTestRoute() {
   
+        // This is the test route to demo code changes going live.
+
         self.get("test") { _ in
-            "Hello, World!"
+            "We made some code changes! Lets deploy the latest version!"
         }
 
     }
