c74fbad147b1739bebb1471b38b3946df5c62ad3
mottzi <sayilir.berken@gmail.com>
2026-05-23 10:17:42 +0200

    Hello, World!

--- Tests/MottziTests/MottziTests.swift
+++ Tests/MottziTests/MottziTests.swift
@@ -1,17 +1,15 @@
 import Testing
 
-@Suite("Mottzi Tests")
-struct MottziTests
-{
-    @Test("Dummy test passes")
-    func dummyTest()
-    {
+@Suite("Tests")
+struct MottziTests {
+
+    @Test("Dummy test #1 (will pass)")
+    func dummyTest() {
         #expect(1 + 1 == 2)
     }
     
-    @Test("Dummy test 2 fails maybe")
-    func dummyTest2()
-    {
+    @Test("Dummy test #2 (will pass)")
+    func dummyTest2() {
         #expect(2 + 2 == 4)
     }
      
