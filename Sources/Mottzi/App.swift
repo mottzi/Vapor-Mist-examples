6ef5cb1daff5355c765735e77ab1740b46c44e2c
Berken Sayilir <sayilir.berken@gmail.com>
2026-07-06 16:45:47 +0200

    lul11

--- Sources/Mottzi/App.swift
+++ Sources/Mottzi/App.swift
@@ -14,9 +14,7 @@ import Vapor
             level: try Logger.Level.detect(from: &env)
         )
         let app = try await Application.make(env)
-        
-        fatalError("test exit!")
-        
+                
         try app.useVariables()
 
         app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
